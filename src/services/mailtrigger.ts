76708e5fcf8823c2c27de27fc4dd3c0947756f43
Pybool <ekoemmanueljavl@gmail.com>
2023-12-04 13:13:08 +0100

    Added try catch to mail service

--- src/services/mailtrigger.ts
+++ src/services/mailtrigger.ts
@@ -1,30 +1,33 @@
-import nodemailer from 'nodemailer';
-import { Options } from 'nodemailer/lib/mailer';
-import { config as dotenvConfig } from 'dotenv';
-dotenvConfig()
+import nodemailer from "nodemailer";
+import { Options } from "nodemailer/lib/mailer";
+import { config as dotenvConfig } from "dotenv";
+dotenvConfig();
+let transporter:any;
+let sendMail:any;
+try {
+  transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_HOST,
+    host: process.env.EMAIL_HOST,
+    port: parseInt(process.env.EMAIL_PORT || "2525"),
+    auth: {
+      user: process.env.EMAIL_HOST_USER,
+      pass: process.env.EMAIL_HOST_PASSWORD,
+    },
+  });
 
-const transporter = nodemailer.createTransport({
-      service: process.env.EMAIL_HOST,
-      host: process.env.EMAIL_HOST,
-      port: parseInt(process.env.EMAIL_PORT || '2525'),
-      auth: {
-        user: process.env.EMAIL_HOST_USER,
-        pass: process.env.EMAIL_HOST_PASSWORD
-      }
-});
-
-const sendMail = (mailOptions: Options) => {
+  sendMail = (mailOptions: Options) => {
     return new Promise((resolve, reject) => {
-      transporter.sendMail(mailOptions, (error:any, info:any) => {
+      transporter.sendMail(mailOptions, (error: any, info: any) => {
         if (error) {
-          console.error('Email error:', error);
-          reject(error); 
+          console.error("Email error:", error);
+          reject(error);
         } else {
-          console.log('Email sent:', info.response);
-          resolve(info); 
+          console.log("Email sent:", info.response);
+          resolve(info);
         }
       });
     });
   };
+} catch {console.log("Error occured in mail")}
 
-export default sendMail
+export default sendMail;
